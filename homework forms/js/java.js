651ad4cc04cba44bec4a65312528b428dbb49b2a
berkanalce <berkan.alce@icloud.com>
2023-02-14 18:14:47 +0100

    java

---
+++ homework forms/js/java.js
@@ -0,0 +1,16 @@
+let name1="bekran alcevski";
+let age=25;
+let job= undefined;
+let gender="male";
+let liveInSkopje=true
+
+let person = {
+name : name1,
+age1 : age,
+job1 : job,
+gender1 : gender    ,
+live: liveInSkopje,
+}
+function funcName(){
+    alert(person.name + " " + person.age1 + " " +  person.job1 + " " + person.gender1  + " " + person.live)
+}
